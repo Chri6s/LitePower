8c58c3917a12d5eeef5887898b1468af14833e19
FrimeX <pistahozak@gmail.com>
2021-05-02 15:29:10 +0200

    Fused Android and Windows version
    I decided to fuse the 2 versions because it was really confusing to work on 2 separate "Projects". I decided to 2 fuse the 2 version together and with some CSS magic make it responsive.

--- Backend/postpuller.php
+++ Backend/postpuller.php
@@ -4,24 +4,20 @@
     }
 </script>
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $current_data = file_get_contents('../data.json');
+    $current_data = file_get_contents('data.json');
 $array_data = json_decode($current_data, true);
 $extra = array(
     'date' => date('m/d'),
     'Time' => date('H:i'),
-    'wph' => $_POST['wph']
+    'wph' => $_GET['wph']
 );
 $array_data[] = $extra;
 $final_data = json_encode($array_data);
-file_put_contents('../data.json', $final_data);
-$finalized_rawjson = file_get_contents('../data.json');
+file_put_contents('data.json', $final_data);
+$finalized_rawjson = file_get_contents('data.json');
 $finalized_json = json_decode($finalized_rawjson, true);
 print_r($finalized_json);
-} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    echo "why are you looking at my postPuller script? Kudos for you, you found it!";
-}
+
 ?>
 <form action="" method="post">
     <p>
---
+++ main.js
@@ -0,0 +1,95 @@
+const WindowsMain = new Module("WindowsMain", "rgba(218, 39, 39)")
+const canvas = document.getElementById("wphcanv").getContext("2d");
+var Chartdata;
+var wphChart = new Chart(canvas, {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            label: 'Watts per hour(WpH)',
+            data: [],
+            backgroundColor: "rgba(218, 39, 39)",
+            borderColor: [
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)'
+            ],
+            borderWidth: 0,
+            pointBackgroundColor: [],
+            pointBorderColor: [],
+            pointBorderWidth: 1,
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 12,
+                        family: 'Ubuntu Mono',
+                    }
+                }
+            }
+        },
+        responsiveAnimationDuration: 1000,
+		elements: {
+			point: {
+				pointStyle: 'circle',
+				rotation: 45,
+				radius: 3,
+			},
+			line: {
+                cubicInterpolationMode: 'monotone',
+                steppedLine: 'before'
+			}
+		},
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 100,
+                    stepSize: 5,
+                    maxTicksLimit: 100,
+                    suggestedMin: 0,
+                    beginAtZero: true,
+                    fontColor: 'rgba(218, 55, 55)',
+                    fontSize: 14,
+                    fontFamily: "Ubuntu Mono",
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontColor: 'rgba(218, 39, 39)',
+                    fontFamily: "Ubuntu Mono",
+                    fontSize: 14,
+                }
+            }]
+        },
+    },
+});
+start();
+async function start() { 
+    var month = new Date().getMonth() + 1;
+    document.title = "Sundata " + new Date().getFullYear() + "/" + month + "/" + new Date().getDate();
+    Chartdata = await fetch("/data.json").then(res => res.json()).then(data => Chartdata = data).catch(error => console.log(error));
+    setupCharts();
+    return;
+}
+function setupCharts() {
+	//setting the colors
+	var maxIndex = wphChart.data.labels.length;
+	for(var i = 0; i < maxIndex; i++) {
+		wphChart.data.labels[i] = Chartdata[i].Time;
+        wphChart.data.datasets[0].pointBorderColor[i] = "rgba(44,44,44,0.66)";
+	}
+    for (var i = 0; i < 7; i++) {
+        if (i > 6) {
+            return;
+        }
+        wphChart.data.datasets[0].data[i] = Chartdata[i].wph;
+    }
+    wphChart.update();
+}
